fa6375ad2f329810847722c1984b3218cd5afd26
ermak-yana <lidl.peperts.stor@gmail.com>
2021-08-20 15:29:30 +0300

    timer-off

--- timer.js
+++ timer.js
@@ -1,17 +1,60 @@
 
-function logger () {
-    // console.log("start");
-};
-let id = null;
-function timerStart () {
-    startData = Date.now();
-    id = setInterval(logger, 1000);
-   
-} ;
+  
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      // timer: document.querySelector('#timer-1'),
+      days: document.querySelector('span[data-value="days"]'),
+      hours: document.querySelector('span[data-value="hours"]'),
+      mins: document.querySelector('span[data-value="mins"]'),
+      secs: document.querySelector('span[data-value="secs"]'),
+    };
+    this.intervalId = null;
+  }
 
-window.addEventListener('DOMContentLoaded', timerStart);
+  start() {
+    const startTime = this.targetDate.getTime();
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      if (deltaTime <= 0) {
+        clearInterval(this.intervalId);
+      }
+      this.getTimeComponents(deltaTime);
+    }, 1000);
+  }
 
-new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Oct 23, 2021'),
-  });
+  // Для подсчета значений используй следующие готовые формулы, где time - разница между targetDate и текущей датой.
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    this.updateTimer(days, hours, mins, secs);
+  }
+
+  // метод принимает число приводит к строке и добавляет к нему знаки, в начало 0 если число меньше 2 - х знаков
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  updateTimer(days, hours, mins, secs) {
+    this.refs.days.textContent = `${days}`;
+    this.refs.hours.textContent = `${hours}`;
+    this.refs.mins.textContent = `${mins}`;
+    this.refs.secs.textContent = `${secs}`;
+  }
+}
+
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Oct 23, 2021'),
+});
+
+timer.start();
